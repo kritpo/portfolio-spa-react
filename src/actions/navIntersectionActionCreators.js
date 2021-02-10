12168a1fac308fe1a783083ae65e0cb883010cbb
Jimmy <jimmy.weng@efrei.net>
2021-02-10 18:20:54 +0100

    fix: optimization of nav intersection observer action

--- src/actions/navIntersectionActionCreators.js
+++ src/actions/navIntersectionActionCreators.js
@@ -4,7 +4,7 @@ import * as ActionTypes from './types';
  * redux action: set the nav intersection observer dependencies
  * @param {object} payload new intersection utilities
  */
-export const updateNavIntersection = payload => ({
+export const updateNavIntersection = ({ ref, inView, entry }) => ({
 	type: ActionTypes.UPDATE_NAV_INTERSECTION,
-	payload
+	payload: { ref, inView, entry }
 });
